8d02c8b9d098c4bfa79b588c9752a64fd1237732
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-12-26 10:48:28 +0200

    Optimize implementation
    PR-URL: https://github.com/metarhia/config/pull/8

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const vm = require('vm');
-const path = require('path');
-const fs = require('fs').promises;
-const common = require('@metarhia/common');
+const { createContext, Script } = require('vm');
+const { extname, basename, join } = require('path');
+const { readdir, readFile } = require('fs').promises;
 
 const USE_STRICT = `'use strict';\n`;
 const SCRIPT_TIMEOUT = 5000;
@@ -14,24 +13,21 @@ class Config {
     this.sections = {};
     this.dir = dir;
     this.mode = mode;
-    this.sandbox = sandbox || vm.createContext({});
+    this.sandbox = sandbox || createContext({});
     this.priority = priority;
     return this.load();
   }
 
   async load() {
-    const files = await fs.readdir(this.dir);
-    if (this.priority) {
-      files.sort((s1, s2) => common.sortComparePriority(this.priority, s1, s2));
-    }
+    const files = await readdir(this.dir);
     const mode = '.' + this.mode;
     const sections = files
       .filter(file => {
-        const fileExt = path.extname(file);
+        const fileExt = extname(file);
         if (fileExt !== '.js') return false;
-        const fileName = path.basename(file, fileExt);
+        const fileName = basename(file, fileExt);
         if (!this.mode) return !fileName.includes('.');
-        const fileMode = path.extname(fileName);
+        const fileMode = extname(fileName);
         if (fileMode) return fileMode === mode;
         const defaultName = `${fileName}${mode}.js`;
         return !files.includes(defaultName);
@@ -42,29 +38,25 @@ class Config {
   }
 
   async loadFile(file) {
-    const configFile = path.join(this.dir, file);
+    const configFile = join(this.dir, file);
     const sectionName = file.substring(0, file.indexOf('.'));
     const exports = await this.createScript(configFile);
     this.sections[sectionName] = exports;
   }
 
   async createScript(fileName) {
-    const src = await fs.readFile(fileName);
+    const src = await readFile(fileName);
     const scriptOptions = {
       filename: fileName,
       lineOffset: -2, // to compensate USE_STRICT wrapper
     };
     const code = USE_STRICT + src;
-    const script = new vm.Script(code, scriptOptions);
+    const script = new Script(code, scriptOptions);
     const runOptions = {
       displayErrors: false,
       timeout: SCRIPT_TIMEOUT, // msec
     };
-    const exports = script.runInContext(this.sandbox, runOptions);
-    if (Object.prototype.toString.call(exports) === '[object Error]') {
-      throw new Error(`Can not create script ${fileName}`);
-    }
-    return exports;
+    return script.runInContext(this.sandbox, runOptions);
   }
 }
 
