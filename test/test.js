2cbb0ca4724740b2f2fdbed181f0d72a11fd59f6
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-19 00:34:24 +0300

    Return promise with sections from constructor
    Closes: https://github.com/metarhia/config/issues/30

--- test/test.js
+++ test/test.js
@@ -6,66 +6,48 @@ const Config = require('../config.js');
 const metatests = require('metatests');
 
 metatests.test('Simple server', async test => {
-  const testConfig = {
-    sections: {
-      server: { transport: 'http', address: '127.0.0.1', ports: 80 },
-    },
-    path: './examples/example1',
-    mode: '',
-    sandbox: {},
-    names: null,
+  const sections = {
+    server: { transport: 'http', address: '127.0.0.1', ports: 80 },
   };
   const config = await new Config('./examples/example1');
-  test.strictSame(config, testConfig);
+  test.strictSame(config, sections);
   test.end();
 });
 
 metatests.test('Server with logger', async test => {
   const sandbox = { Duration: common.duration };
-  const testConfig = {
-    sections: {
-      server: { transport: 'http', address: '127.0.0.1', ports: 80 },
-      logger: {
-        enabled: true,
-        keepDays: 100,
-        writeInterval: 3000,
-        writeBuffer: 65536,
-        toStdout: ['system', 'fatal', 'error'],
-      },
+  const sections = {
+    server: { transport: 'http', address: '127.0.0.1', ports: 80 },
+    logger: {
+      enabled: true,
+      keepDays: 100,
+      writeInterval: 3000,
+      writeBuffer: 65536,
+      toStdout: ['system', 'fatal', 'error'],
     },
-    path: './examples/example2',
-    mode: '',
-    sandbox,
-    names: null,
   };
   vm.createContext(sandbox);
   const config = await new Config('./examples/example2', { sandbox });
-  test.strictSame(config, testConfig);
+  test.strictSame(config, sections);
   test.end();
 });
 
 metatests.test('Application server', async test => {
   const sandbox = { Duration: common.duration };
-  const testConfig = {
-    sections: {
-      application: { name: 'Application name' },
-      gateway: { host: '10.0.0.1', port: 2000 },
-      dependencies: {
-        internal: ['fs', 'path', 'http'],
-        external: ['@metarhia/common', 'metasync', 'eslint'],
-      },
-      server: { transport: 'http', address: '127.0.0.1', ports: 8080 },
-      timeouts: { cache: 30000, relpy: 5000, query: 3000 },
+  const sections = {
+    application: { name: 'Application name' },
+    gateway: { host: '10.0.0.1', port: 2000 },
+    dependencies: {
+      internal: ['fs', 'path', 'http'],
+      external: ['@metarhia/common', 'metasync', 'eslint'],
     },
-    path: './examples/example3',
-    mode: 'test',
-    sandbox,
-    names: null,
+    server: { transport: 'http', address: '127.0.0.1', ports: 8080 },
+    timeouts: { cache: 30000, relpy: 5000, query: 3000 },
   };
   vm.createContext(sandbox);
   const options = { sandbox, mode: 'test' };
   const config = await new Config('./examples/example3', options);
-  test.strictSame(config, testConfig);
+  test.strictSame(config, sections);
   test.end();
 });
 
@@ -81,35 +63,23 @@ metatests.test('Incorrect path error', async test => {
 });
 
 metatests.test('Specified sections', async test => {
-  const testConfig = {
-    sections: {
-      application: { name: 'Application name' },
-    },
-    path: './examples/example3',
-    mode: '',
-    sandbox: {},
-    names: ['application', 'gateway'],
+  const sections = {
+    application: { name: 'Application name' },
   };
   const names = ['application', 'gateway'];
   const config = await new Config('./examples/example3', names);
-  test.strictSame(config, testConfig);
+  test.strictSame(config, sections);
   test.end();
 });
 
 metatests.test('Specified sections with options', async test => {
-  const testConfig = {
-    sections: {
-      application: { name: 'Application name' },
-      gateway: { host: '10.0.0.1', port: 2000 },
-    },
-    path: './examples/example3',
-    mode: 'test',
-    sandbox: {},
-    names: ['application', 'gateway'],
+  const sections = {
+    application: { name: 'Application name' },
+    gateway: { host: '10.0.0.1', port: 2000 },
   };
   const options = { mode: 'test' };
   const names = ['application', 'gateway'];
   const config = await new Config('./examples/example3', options, names);
-  test.strictSame(config, testConfig);
+  test.strictSame(config, sections);
   test.end();
 });
