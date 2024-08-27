c4fdea618e215e030069a9f2fd66e41a4a1dd482
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-08-27 06:14:00 +0300

    Fix tests

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,17 @@
 
 const init = require('eslint-config-metarhia');
 
-module.exports = [...init];
+module.exports = [
+  ...init,
+  {
+    files: ['examples/**/*.js'],
+    rules: {
+      strict: 'off',
+    },
+    languageOptions: {
+      globals: {
+        duration: true,
+      },
+    },
+  },
+];
