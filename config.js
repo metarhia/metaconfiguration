c51f8c6d73fb54245ab555e87a0da2eb731894d3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-09-09 23:05:02 +0300

    Rewrite to improve but changing external API

--- config.js
+++ config.js
@@ -1,49 +1,57 @@
 'use strict';
 
-const metavm = require('metavm');
 const path = require('node:path');
 const fsp = require('node:fs').promises;
+const { createContext, readScript } = require('metavm');
 
 class Config {
+  #path;
+  #names = [];
+  #mode;
+  #context;
+  #sections = {};
+
   constructor(dirPath, options = {}) {
     const { names, mode, context } = options;
-    this.sections = {};
-    this.path = dirPath;
-    this.names = names || null;
-    this.mode = mode || '';
-    this.context = context || metavm.createContext();
-    return this.load();
+    this.#path = dirPath;
+    if (names) this.#names = names;
+    this.#mode = mode || '';
+    this.#context = context || createContext();
+    return this.#load();
   }
 
-  async load() {
-    const files = await fsp.readdir(this.path);
-    const mode = '.' + this.mode;
-    const names = [];
+  static async create(dirPath, options) {
+    return new Config(dirPath, options);
+  }
+
+  async #load() {
+    const files = await fsp.readdir(this.#path);
+    const mode = '.' + this.#mode;
+    const names = this.#names;
+    const pending = [];
     for (const file of files) {
       const fileExt = path.extname(file);
       if (fileExt !== '.js') continue;
       const fileName = path.basename(file, fileExt);
       const fileMode = path.extname(fileName);
       const sectionName = path.basename(fileName, fileMode);
-      if (this.names && !this.names.includes(sectionName)) continue;
-      if (!this.mode && fileName.includes('.')) continue;
+      if (names.length > 0 && !names.includes(sectionName)) continue;
+      if (!this.#mode && fileName.includes('.')) continue;
       if (fileMode && fileMode !== mode) continue;
       const defaultName = `${fileName}${mode}.js`;
-      if (!files.includes(defaultName)) names.push(this.loadFile(file));
+      if (!files.includes(defaultName)) pending.push(this.#loadFile(file));
     }
-    await Promise.all(names);
-    return this.sections;
+    await Promise.all(pending);
+    return this.#sections;
   }
 
-  async loadFile(file) {
-    const configFile = path.join(this.path, file);
+  async #loadFile(file) {
+    const configFile = path.join(this.#path, file);
     const sectionName = file.substring(0, file.indexOf('.'));
-    const options = { context: this.context };
-    const { exports } = await metavm.readScript(configFile, options);
-    this.sections[sectionName] = exports;
+    const options = { context: this.#context };
+    const { exports } = await readScript(configFile, options);
+    this.#sections[sectionName] = exports;
   }
 }
 
-const readConfig = (dirPath, options) => new Config(dirPath, options);
-
-module.exports = { Config, readConfig };
+module.exports = { Config };
