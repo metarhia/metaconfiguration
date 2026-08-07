bc11cf5e7fae75b8827b34dfa683ca14bf8c1bc2
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2026-08-07 22:59:35 +0300

    Refactor tests

--- test/unittests.js
+++ test/unittests.js
@@ -3,8 +3,15 @@
 const { test } = require('node:test');
 const { strictEqual, rejects, deepStrictEqual } = require('node:assert');
 const vm = require('node:vm');
+
 const metautil = require('metautil');
 const { Config } = require('../config.js');
+
+const createDurationContext = () => {
+  const context = { duration: metautil.duration };
+  vm.createContext(context);
+  return context;
+};
 
 test('Config basic loading', async () => {
   const config = await Config.create('./examples/example1');
@@ -21,72 +28,65 @@ test('Config factory method', async () => {
 });
 
 test('Server with logger', async () => {
-  const context = { duration: metautil.duration };
-  vm.createContext(context);
+  const context = createDurationContext();
   const options = { context };
   const config = await Config.create('./examples/example2', options);
 
-  // Test server config
   strictEqual(config.server.transport, 'http');
   strictEqual(config.server.address, '127.0.0.1');
   strictEqual(config.server.ports, 80);
 
-  // Test logger config
   strictEqual(config.logger.enabled, true);
   strictEqual(config.logger.keepDays, 100);
   strictEqual(config.logger.writeInterval, 3000);
   strictEqual(config.logger.writeBuffer, 65536);
-  strictEqual(config.logger.toStdout.length, 3);
-  strictEqual(config.logger.toStdout[0], 'system');
-  strictEqual(config.logger.toStdout[1], 'fatal');
-  strictEqual(config.logger.toStdout[2], 'error');
+  const { toStdout } = config.logger;
+  strictEqual(toStdout.length, 3);
+  strictEqual(toStdout[0], 'system');
+  strictEqual(toStdout[1], 'fatal');
+  strictEqual(toStdout[2], 'error');
 });
 
 test('Application server with test mode', async () => {
-  const context = { duration: metautil.duration };
-  vm.createContext(context);
+  const context = createDurationContext();
   const options = { context, mode: 'test' };
   const config = await Config.create('./examples/example3', options);
 
-  // Test application config
   strictEqual(config.application.name, 'Application name');
 
-  // Test gateway config
   strictEqual(config.gateway.host, '10.0.0.1');
   strictEqual(config.gateway.port, 2000);
 
-  // Test dependencies config
-  strictEqual(config.dependencies.internal.length, 3);
-  strictEqual(config.dependencies.internal[0], 'fs');
-  strictEqual(config.dependencies.internal[1], 'path');
-  strictEqual(config.dependencies.internal[2], 'http');
-  strictEqual(config.dependencies.external.length, 3);
-  strictEqual(config.dependencies.external[0], 'metautil');
-  strictEqual(config.dependencies.external[1], 'metasync');
-  strictEqual(config.dependencies.external[2], 'eslint');
+  const { internal, external } = config.dependencies;
+  strictEqual(internal.length, 3);
+  strictEqual(internal[0], 'fs');
+  strictEqual(internal[1], 'path');
+  strictEqual(internal[2], 'http');
+  strictEqual(external.length, 3);
+  strictEqual(external[0], 'metautil');
+  strictEqual(external[1], 'metasync');
+  strictEqual(external[2], 'eslint');
 
-  // Test server config
   strictEqual(config.server.transport, 'http');
   strictEqual(config.server.address, '127.0.0.1');
   strictEqual(config.server.ports, 8080);
 
-  // Test timeouts config
   strictEqual(config.timeouts.cache, 30000);
   strictEqual(config.timeouts.reply, 5000);
   strictEqual(config.timeouts.query, 3000);
 });
 
 test('Application server with debug mode', async () => {
-  const context = { duration: metautil.duration };
-  vm.createContext(context);
+  const context = createDurationContext();
   const options = { context, mode: 'debug' };
   const config = await Config.create('./examples/example3', options);
 
   strictEqual(config.application.name, 'Application name: Debug mode');
 
-  strictEqual(config.dependencies.internal.length, 5);
-  strictEqual(config.dependencies.internal[3], 'v8');
-  strictEqual(config.dependencies.internal[4], 'timers');
+  const { internal } = config.dependencies;
+  strictEqual(internal.length, 5);
+  strictEqual(internal[3], 'v8');
+  strictEqual(internal[4], 'timers');
 
   strictEqual(config.server.transport, 'http');
   strictEqual(config.server.address, '127.0.0.1');
